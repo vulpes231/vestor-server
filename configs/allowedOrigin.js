4eba8492532ddabce3828a4a69ced26bf67b128f
Vulpes231 <whykayshot@gmail.com>
2025-01-20 01:51:37 +0100

    allowd

--- configs/allowedOrigin.js
+++ configs/allowedOrigin.js
@@ -2,6 +2,7 @@ const allowedOrigins = [
   "https://vestorr.netlify.app",
   "http://localhost:5173",
   "https://vestor.markets",
+  "https://admin.vestor.markets",
 ];
 
 module.exports = { allowedOrigins };
